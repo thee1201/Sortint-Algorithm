8b423fdc38b96e66e6cf8a189a2e5351f9a0def8
thee1201 <165997100+thee1201@users.noreply.github.com>
2024-07-09 22:45:30 +0900

    Add files via upload
    선택정렬 구현 코드

---
+++ SelectionSort.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define MAX 100
+
+void selectionSort(int array[], int N);
+void swap(int* a, int* b);
+void inputArray(int array[], int N);
+void printArray(int array[], int N);
+
+int main(void) {
+	int a[MAX];
+	int N;
+
+	printf("Array Size: "); scanf("%d", &N);
+	inputArray(a, N);
+
+	selectionSort(a, N);
+
+	printArray(a, N);
+
+	return 0;
+}
+
+void swap(int* a, int* b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void inputArray(int array[], int N) {
+	printf("Array: ");
+	for (int i = 0; i < N; i++)
+		scanf("%d", &array[i]);
+}
+
+void selectionSort(int array[], int N) {
+	int mindex;
+	for (int i = 0; i < N; i++) {
+		mindex = i;
+		for (int j = i; j < N; j++) {
+			if (array[j] < array[mindex])
+				mindex = j;
+		}
+		swap(&array[i], &array[mindex]);
+	}
+}
+
+void printArray(int array[], int N) {
+	printf("\nSelection Srot Array: ");
+	for (int i = 0; i < N; i++)
+		printf("%d ", array[i]);
+	printf("\n");
+}
